cc531e43119080c7407eb4d5da380fcc9077e5b3
CinthiaVilcachagua <cv.ccori@gmail.com>
2019-07-10 10:13:15 -0500

    preparando el entorno para trabajar con react, inicializando con firebase-instalando dependencias

---
+++ albums/src/inicializacion/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDDDrlAsoTM8UGgaAYiFhP98WU1Zrc1bgo",
+    authDomain: "album-react-js.firebaseapp.com",
+    databaseURL: "https://album-react-js.firebaseio.com",
+    projectId: "album-react-js",
+    storageBucket: "album-react-js.appspot.com",
+    messagingSenderId: "492472539775",
+    appId: "1:492472539775:web:8e099b4dac22c1da"
+  };
+
+  fiirebase.initializeApp(config);
+  export default firebase;
